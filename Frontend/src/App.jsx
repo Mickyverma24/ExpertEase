19159fb1ed6b8b8db92e16550a8ec606ae28f054
Ravi verma <vermaravi8809@gmail.com>
2024-03-29 13:27:18 +0530

    Adding react router dom in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import Login from './pages/login/Login'
 import Home from './pages/home/Home'
 import SingUp from './pages/signup/SingUp'
-
+import {Routes,Route} from 'react-router-dom'
 function App() {
  return (
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <Login/> */}
-      <Home/>
-      {/* <SingUp/> */}
+      <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/login' element={<Login/>}/>
+         <Route path='/signup' element={<SingUp/>}/>
+      </Routes>
+      
     </div>
   )
 }
--- Frontend/src/pages/signup/SingUp.jsx
+++ Frontend/src/pages/signup/SingUp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Gender from "./Gender";
+import { Link } from "react-router-dom";
 
 const SingUp = () => {
   return (
@@ -52,12 +53,12 @@ const SingUp = () => {
             />
           </div>
           <Gender/>
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="text-sm  hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             Already have an account?
-          </a>
+          </Link>
 
           <div>
             <button className="btn btn-block btn-sm mt-2">SignIn</button>
