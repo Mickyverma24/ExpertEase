f69c465dc7b499301d51f3fce17aadccc243f5a9
Ravi verma <vermaravi8809@gmail.com>
2024-04-02 13:56:16 +0530

    prod ready

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -17,14 +17,14 @@ export const getReceiverSocketId = (receiverId) =>{
     return userSocketMap[receiverId];
 }
 io.on("connection", (socket) => {
-  console.log("A user connected..", socket.id);
+  // console.log("A user connected..", socket.id);
   const userId = socket.handshake.query.userId;
   if (userId != "undefined") userSocketMap[userId] = socket.id;
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
   // socket.on() is used to listen to the events.can use used both on client and server side
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
+    // console.log("User disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,31 @@
-import './App.css'
-import Login from './pages/login/Login'
-import Home from './pages/home/Home'
-import SingUp from './pages/signup/SingUp'
-import {Routes,Route, Navigate} from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import { useAuthContext } from './contexts/AuthContext'
+import "./App.css";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import SingUp from "./pages/signup/SingUp";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./contexts/AuthContext";
 function App() {
-  const {authUser} = useAuthContext();
-  console.log(authUser)
- return (
+  const { authUser } = useAuthContext();
+  return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-         <Route path='/' element={authUser ?<Home/> : <Navigate to={"/login"}/>}/>
-         <Route path='/login' element={authUser ?<Navigate to ='/'/> :<Login/>}/>
-         <Route path='/signup' element={authUser ?<Navigate to ='/'/> : <SingUp/>}/>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SingUp />}
+        />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
